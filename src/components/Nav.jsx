a59876d4f09c1f02ae552b35d0d9adc2ace4de73
*vivek <vrvivekofficial@gmail.com>
2024-06-07 16:20:51 +0530

    nav updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
+import { Form, NavLink } from "react-router-dom";
+import logoMark from "../assets/logomark.svg";
 const Nav = ({ userName }) => {
-  return <nav>{userName}</nav>;
+  return (
+    <nav>
+      <NavLink to="/" aria-label="Go to home">
+        <img src={logoMark} alt="logo" />
+        <span>HomeBudget</span>
+      </NavLink>
+      {
+        userName && (
+          <Form method="post" action="/logout">
+            <button type="submit" className="btn btn--warning"><span>Delete User</span></button>
+            
+          </Form>
+        )
+      }
+    </nav>
+  );
 };
 
 export default Nav;
