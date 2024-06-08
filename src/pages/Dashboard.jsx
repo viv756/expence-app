1634765519dc1060d55d95512a94695fdfacf2ff
*vivek <vrvivekofficial@gmail.com>
2024-06-08 15:43:23 +0530

    addbudget form created

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,19 @@
 import { useLoaderData } from "react-router-dom";
-
+// components
+import Intro from "../components/Intro";
+import AddBudgetForm from "../components/AddBudgetForm";
 // helper functions
 import { fetchdata } from "../helpers";
-import Intro from "../components/Intro";
+// Library
 import { toast } from "react-toastify";
+
 
 // Loader
 export function dashboardLoader() {
   const userName = fetchdata("userName");
-  return { userName };
+  const budjets = fetchdata("budgets");
+
+  return { userName, budjets };
 }
 
 // action
@@ -16,17 +21,37 @@ export async function dashboardAction({ request }) {
   const data = await request.formData();
   // const userName = data.get("userName")
   const formData = Object.fromEntries(data);
- 
+
   try {
     localStorage.setItem("userName", JSON.stringify(formData.userName));
-    return toast.success(`Welcome, ${formData.userName}`)
+    return toast.success(`Welcome, ${formData.userName}`);
   } catch (e) {
-    throw new Error("There was a problem creating your account")
+    throw new Error("There was a problem creating your account");
   }
 }
 const Dashboard = () => {
-  const { userName } = useLoaderData();
-  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
+  const { userName, budgets } = useLoaderData();
+  return (
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome back,<span className="accent">{userName}</span>
+          </h1>
+          <div className="grid-sm">
+            {/* {budjets}?():()*/}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
